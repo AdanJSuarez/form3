45f928726d75246eb15cd210f2e58aa438ccb4b1
Adan J. Suarez <adanjsuarez@gmail.com>
2023-02-02 15:28:23 -0500

    feat: Implemented Create and Connection

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Form3 Homework
 
 Form3 homework. REST client.
+
+Ref:
+https://github.com/form3tech-oss/interview-accountapi
+https://github.com/form3tech-oss/go-form3
--- internal/connection.go
+++ internal/connection.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"io"
 	"net/http"
 	"time"
 )
@@ -11,20 +12,35 @@ const POST = "POST"
 type URL string
 
 type Connection struct {
-	url    string
+	url    URL
 	client http.Client
 }
 
-func NewConnection(URL string) *Connection {
+func NewConnection(method, url URL) *Connection {
 	client := http.Client{
 		Timeout: timeout,
 	}
-	r, err := http.NewRequest(POST, "", nil)
-	if err != nil {
-		//
-	}
-	r.Header.Add("", "")
-	// res, err := client.Do(r)
 
-	return &Connection{url: URL, client: client}
+	return &Connection{url: url, client: client}
+}
+
+func (c *Connection) Post(body io.Reader) (*http.Response, error) {
+	req := request(POST, c.url, body)
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func request(method string, url URL, body io.Reader) *http.Request {
+	r, err := http.NewRequest(method, string(url), body)
+	if err != nil {
+		return nil
+	}
+	// TODO: Add needed header
+	r.Header.Add("", "")
+	return r
 }
